b69de2479d3e2ba6f4b1b42625db2ac8315be099
Alan <1403528138@qq.com>
2020-02-05 19:05:41 +0800

    支持背景（颜色、图片）文字颜色的皮肤变换

--- app/src/main/java/com/example/dynamicskindemo/skin/SkinEngine.java
+++ app/src/main/java/com/example/dynamicskindemo/skin/SkinEngine.java
@@ -1,17 +1,20 @@
 package com.example.dynamicskindemo.skin;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 
 import java.io.File;
 import java.lang.reflect.Method;
 
 import androidx.core.content.ContextCompat;
 
+@SuppressLint("StaticFieldLeak")
 public class SkinEngine {
 
     //饿汉模式
@@ -29,37 +32,32 @@ public class SkinEngine {
     }
 
     public void init(Context context) {
-        mContext = context.getApplicationContext();
         //使用application的目的是，如果万一传进来的是activity对象
         //那么它被静态对象instance所持有，这个Activity就无法释放了
+        mContext = context.getApplicationContext();
     }
 
-    /*
-        加载外部资源包
+    /**
+     * 加载外部资源包
+     * @param path 外部传入的apk文件名
      */
-    public void load(final String path) { //path是外部传入的apk文件名
+    public void load(final String path) {
         File file = new File(path);
         if (!file.exists()) {
             return;
         }
-        //取得PackageManager引用
-        PackageManager mPackageManager = mContext.getPackageManager();
-        //PackageInfo中包含着应用程序的总体信息
-        PackageInfo mInfo = mPackageManager.getPackageArchiveInfo(path, PackageManager.GET_ACTIVITIES);
-        mPackageName = mInfo.packageName; //将包名储存起来
-        AssetManager assetManager; //资源管理器
-        //通过反射获取AssetManager用来加载外部apk资源
+        PackageManager packageManager = mContext.getPackageManager();
+        PackageInfo info = packageManager.getPackageArchiveInfo(path, PackageManager.GET_ACTIVITIES);
+        if (info != null) mPackageName = info.packageName;
+        AssetManager assetManager;
         try {
-            //因为addAssetPath为私有方法，如果使用就需要通过反射的方式
             assetManager = AssetManager.class.newInstance();
             Method addAssetPath = assetManager.getClass().getMethod("addAssetPath", String.class);
             addAssetPath.setAccessible(true);
-            addAssetPath.invoke(assetManager, path); //反射执行方法，设置加载路径
-            mResources = new Resources(assetManager   //资源管理器
-                    , mContext.getResources().getDisplayMetrics() //屏幕参数
-                    , mContext.getResources().getConfiguration()); //资源配置
-            //最终创建从一个可以加载外部资源文件的Resource对象
-
+            addAssetPath.invoke(assetManager, path);
+            mResources = new Resources(assetManager
+                    , mContext.getResources().getDisplayMetrics()
+                    , mContext.getResources().getConfiguration());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -68,7 +66,6 @@ public class SkinEngine {
 
     /**
      * 提供外部资源包里的颜色
-     *
      * @param resId
      * @return
      */
@@ -76,8 +73,7 @@ public class SkinEngine {
         if (mResources == null) {
             return resId;
         }
-        String resName = mResources.getResourceEntryName(resId);
-        int outResId = mResources.getIdentifier(resName, "color", mPackageName);
+        int outResId = getIdentifier(resId);
         if (outResId == 0) {
             return resId;
         }
@@ -90,16 +86,32 @@ public class SkinEngine {
      * @param resId
      * @return
      */
-    public Drawable getDrawable(int resId) { //获取图片
+    public Drawable getDrawable(int resId) {
         if (mResources == null) {
             return ContextCompat.getDrawable(mContext, resId);
         }
-        String resName = mResources.getResourceEntryName(resId);
-        int outResId = mResources.getIdentifier(resName, "drawable", mPackageName);
+        int outResId = getIdentifier(resId);
         if (outResId == 0) {
             return ContextCompat.getDrawable(mContext, resId);
         }
         return mResources.getDrawable(outResId);
+    }
+
+    public Object getBackground(int resId){
+        String typeName = mResources.getResourceTypeName(resId);
+        if (TextUtils.equals(typeName,"drawable")){
+            return getDrawable(resId);
+        }else if (TextUtils.equals(typeName,"color")){
+            return getColor(resId);
+        }
+        return null;
+    }
+
+    private int getIdentifier(int resId) {
+        String resName = mResources.getResourceEntryName(resId);  //名称，例如ic_launcher
+        String resType = mResources.getResourceTypeName(resId);   //类型，例如drawable
+        int skinId = mResources.getIdentifier(resName, resType, mPackageName);
+        return skinId;
     }
 
 
--- app/src/main/java/com/example/dynamicskindemo/skin/SkinFactory.java
+++ app/src/main/java/com/example/dynamicskindemo/skin/SkinFactory.java
@@ -2,6 +2,7 @@ package com.example.dynamicskindemo.skin;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -15,10 +16,13 @@ import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.view.ViewCompat;
 
 public class SkinFactory implements LayoutInflater.Factory2 {
 
@@ -29,9 +33,7 @@ public class SkinFactory implements LayoutInflater.Factory2 {
     //我自定义的List，缓存所有可变肤view
     private List<SkinView> mListCacheSkinView = new ArrayList<>();
 
-    /*
-        给外部提供一个set方法
-     */
+
     public void setDelegate(AppCompatDelegate delegate) {
         mDelegate = delegate;
     }
@@ -39,11 +41,11 @@ public class SkinFactory implements LayoutInflater.Factory2 {
     @Nullable
     @Override
     public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attributeSet) {
-        //关键点1：执行系统代码里的创建View的过程，我们只想加入自己的操作，并不是要全盘接管
+        //执行系统代码里的创建View的过程，我们只想加入自己的操作，并不是要全盘接管
         //系统创建出来的有时候会为空
         View view = mDelegate.createView(parent, name, context, attributeSet);
         if (view == null) {
-            if (-1 == name.indexOf('.')) { //不包含，说明不带包名，我们帮他添上
+            if (-1 == name.indexOf('.')) {
                 view = createViewByPrefix(context, name, prefixs, attributeSet);
             } else {
                 view = createViewByPrefix(context, name, null, attributeSet);
@@ -84,9 +86,8 @@ public class SkinFactory implements LayoutInflater.Factory2 {
             skinView.view = view;
             skinView.attrsMap = attrMap;
             mListCacheSkinView.add(skinView);  //将可换肤的view，放到mListCacheSkinView中
-            Log.d(TAG, "collectSkinView: 收集view : " + skinView.view.toString());
+            //Log.d(TAG, "collectSkinView: 收集view : " + skinView.view.toString());
         }
-
         typedArray.recycle();
     }
 
@@ -106,41 +107,36 @@ public class SkinFactory implements LayoutInflater.Factory2 {
         /**
          * 真正的换肤操作
          */
-        public void changeSkin() {
-            if (!TextUtils.isEmpty(attrsMap.get("background"))) { //属性名，例如 background 、textColor 、text
-                int bgId = Integer.parseInt(attrsMap.get("background").substring(1)); //属性类别，比如drawable 、 color
-                //这个值，在app的一次运行中，不会发生变化
-                String attrType = view.getResources().getResourceTypeName(bgId);
-                if (TextUtils.equals(attrType, "drawable")) { //区分drawable和color
-                    view.setBackground(SkinEngine.getInstance().getDrawable(bgId));
-                } else if (TextUtils.equals(attrType, "color")) {
-                    view.setBackgroundColor(SkinEngine.getInstance().getColor(bgId));
+        private void changeSkin() {
+            for (Map.Entry<String,String> entry : attrsMap.entrySet()){
+                String attrStr = entry.getValue().substring(1);
+                switch (entry.getKey()){
+                    case "background":
+                        Object bgValue = SkinEngine.getInstance().getBackground(Integer.parseInt(attrStr));
+                        if (bgValue instanceof Drawable){
+                            view.setBackground((Drawable) bgValue);
+                        }else if (bgValue instanceof Integer){
+                            view.setBackgroundColor((Integer) bgValue);
+                        }
+                        break;
+                    case "textColor":
+                        ((TextView) view).setTextColor(Integer.parseInt(attrStr));
+                        break;
+                    default:
+                        break;
                 }
             }
-
-            if (view instanceof TextView) {
-                if (!TextUtils.isEmpty(attrsMap.get("textColor"))) {
-                    int textColorId = Integer.parseInt(attrsMap.get("textColor").substring(1));
-                    ((TextView) view).setTextColor(SkinEngine.getInstance().getColor(textColorId));
-                }
-            }
-
 
         }
-
     }
 
-    // 下面的逻辑摘抄自源码,AppCompatViewInflater类源码里搜索：view = createViewFromTag(context, name, attrs);
     static final Class<?>[] mConstructorSignature = new Class[]{Context.class, AttributeSet.class};
-    //安卓里面控件的包名，就这么3种,这个变量是为了下面代码里，反射创建类的class而预备的
     static final String[] prefixs = new String[]{
             "android.widget.",
             "android.view.",
             "android.webkit."
     };
-    //用映射，将View的反射构造函数都存起来
     private static final HashMap<String, Constructor<? extends View>> sConstructorMap = new HashMap<>();
-    //View的构造函数的2个实参对象
     final Object[] mConstructorArgs = new Object[2];
 
     /**
@@ -162,7 +158,7 @@ public class SkinFactory implements LayoutInflater.Factory2 {
                 if (prefixs != null && prefixs.length > 0) {
                     for (String prefix : prefixs) {
                         clazz = context.getClassLoader().loadClass(
-                                prefix != null ? (prefix + name) : name).asSubclass(View.class);    //控件
+                                prefix != null ? (prefix + name) : name).asSubclass(View.class);
                         if (clazz != null) break;
                     }
                 } else {
@@ -173,19 +169,18 @@ public class SkinFactory implements LayoutInflater.Factory2 {
                 if (clazz == null) {
                     return null;
                 }
-                constructor = clazz.getConstructor(mConstructorSignature);  //拿到 构造方法，
+                constructor = clazz.getConstructor(mConstructorSignature);
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
             }
             constructor.setAccessible(true);
-            sConstructorMap.put(name, constructor); //然后缓存起来，下次再用，就直接从内存中去取
+            sConstructorMap.put(name, constructor);
         }
         Object[] args = mConstructorArgs;
         args[1] = attrs;
         try {
-            //通过反射创建View对象
-            final View view = constructor.newInstance(args);    //执行构造函数，拿到View对象
+            final View view = constructor.newInstance(args);
             return view;
         } catch (Exception e) {
             e.printStackTrace();
